c674f3c9d40b3687563e204c8bc61793602864e9
jjjjjw910911 <jjjjjw910911@letsee.io>
2023-05-10 12:51:04 +0900

    [feat] ConfigModule 설정해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,12 @@
-import { Controller, Get, Post, Res } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('user') // GET /abc/user
-  getUser(): string {
-    const user = this.appService.getUser();
-    return user;
-  }
-
-  @Post('user') // POST /abc/user
-  postUser(): string {
-    this.appService.postUser();
-    return { code: 'SUCCESS', data: user };
-  }
-
-  @Post('user') // POST /abc/user
-  postUser(): string {
-    this.appService.postUser();
-    return { code: 'SUCCESS', data: 'hello' };
+  @Get('hello') //
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,13 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  async getUser(): string {
-    const user = await User.findOne();
-    return user;
-  }
-
-  async postUser(): string {
-    const user = await User.create();
-    return user;
+  getHello() {
+    console.log(process.env.SECRET);
+    return process.env.SECRET;
   }
 }
